dec6a028ee8c4d67ee25d6bc89ca613ea26af9b3
sheetal <135092130+sheetalmasih@users.noreply.github.com>
2024-03-24 00:28:02 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,120 @@
+// 1. Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+
+// let a=4;
+// let b=2;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// ****************************************************
+
+// 2. Take two numbers and only print the integer part when a is divided by b.
+
+// let a=12;
+// let b=5;
+// console.log(Math.floor(a/b))
+
+// ******************************************************
+
+// 3. Take two numbers a,b and print the exponential power of the first number raised by the second.
+
+// let a=4;
+// let b=3;
+// console.log(a**b);
+
+// *******************************************************
+
+// 4. Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+
+// let a=11;
+// let b=3;
+// console.log(a/b);
+
+// *************************************************************
+
+// 5. Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+
+// let markA = 78;
+// let markB = 82;
+// let markC = 75;
+// let markD = 85;
+// let markE = 89;
+// let average = (markA + markB + markC + markD + markE) / 5;
+// console.log("Average marks:", average);
+
+// *************************************************************
+
+// 7. Take a number and print out the last digit of the number.
+
+// let digit=23458;
+// let last=digit%10
+// console.log(last);
+
+
+// ******************************************************************
+
+// 8. Take a 2 digit number and return the first and last digits.
+
+// let digit= 45;
+// let first=Math.floor(digit/10);
+// let last=digit%10;
+// console.log("first digit",first);
+// console.log("last digit",last);
+
+// *******************************************************************
+
+// 9. Write a JavaScript program to find the largest of three numbers.
+
+// let a=34;
+// let b=984;
+// let c=784;
+// if (a>b && a>c){
+//     console.log(a)
+// }
+// else if (b>a && b>c){
+//     console.log(b)
+// }
+// else{
+//     console.log(c)
+// }
+
+// *****************************************************************
+
+// 10. Write a JavaScript program to check whether a number is even or odd.
+
+// let number=31;
+// if (number%2===0){
+//     console.log("Even",number)
+// }
+// else{
+//     console.log("Odd",number)
+// }
+
+// ***************************************************************************
+
+// 11. Write a JavaScript program to check whether a given number is prime or not.
+// let prime=5;
+// let count=0;
+// for(let i=1;i<=prime;i++){
+//     if(prime%i==0){
+//         count++
+//     }
+// }
+// if(count==2){
+//     console.log("this is prime number:",prime)
+// }
+// else{
+//     console.log("this is not a prime number:",prime)
+// }
+
+
+// *******************************************************************
+
+// 12. Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+
+// let Celsius=23;
+// let Fahrenheit=12;
+// console.log("celsius to fahrenheit:",(Celsius*9/5)+32)
+// console.log("fahrenheit to celsius:",(Fahrenheit-32)*9/5)
